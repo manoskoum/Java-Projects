ebb9c2baf7669bff649e582e5142f0582f211739
Emmanouil Koumentakis <manoskoume@gmail.com>
2025-09-24 13:19:15 +0300

    Update Main.java

--- Pet Care Scheduler/src/Main.java
+++ Pet Care Scheduler/src/Main.java
@@ -7,8 +7,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
